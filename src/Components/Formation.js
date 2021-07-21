bd080ccc49044732f134401c405040ffa3eb6885
said.T <tarhisaid@gmail.com>
2021-07-21 15:55:21 +0100

    sauvgarde Toujours en Formation

--- src/Components/Formation.js
+++ src/Components/Formation.js
@@ -10,7 +10,7 @@ function Formation({addFormation,setAddFormation}) {
         <div className="formation">Formation</div>
         <div id="myFormation" className="myFormation" >
             {addFormation.map((element)=>(
-            <div key={element.index} >
+            <div key={element.index} className="uneFormation" >
                 <label htmlFor="dateDebut">DE</label><input id="dateDebut" type="date" style={{position:"relative",}}></input> <label htmlFor="dateFin">A</label> <input id="dateFin" type="date"></input>
                 <textarea style={{width:"63%"}} > Specialite</textarea>
                 <label >Organisme <textarea style={{width:"65%",backgroundColor:"transparent",color:"blue",fontSize:"14px"}}></textarea></label>
